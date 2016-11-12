32a2b29111f1726d0edee9d74f8c50f3f6520a2a
DESKTOP-2KU5P4G\yurik <pyurik44@gmail.com>
2016-11-12 22:13:23 +0300

    Start

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        {title: 'Javacript: The Good Parts', pages: 101},
+        {title: 'Harry Potter', pages: 300},
+        {title: 'The Dark Tower', pages: 400},
+        {title: 'Eloquent Ruby', pages: 23}
+    ]
+}
