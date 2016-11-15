b74e52dc7863ba244f156683baa733a5e63b89bd
DESKTOP-1037RDH\Yurik <pyurik44@gmail.com>
2016-11-15 16:10:51 +0300

    все нормально работает

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,18 @@
+import axios from 'axios';
+
 const API_KEY = '2b76d680bc3d595d0d890976b809fd1d';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    console.log('Request:', reguest)
+
+    return{
+      type: FETCH_WEATHER,
+      payload: request
+    };
+}
