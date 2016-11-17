f1f8730683d330967459aff54943b28b838180c3
DESKTOP-2KU5P4G\yurik <pyurik44@gmail.com>
2016-11-17 23:45:05 +0300

    all corrections

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -3,8 +3,7 @@ import { connect } from 'react-redux';
 
 class WeatherList extends Component {
   renderWeather(cityData){
-    const name = cityData.city.name;
-
+      const name = cityData.city.name;
     return(
       <tr key={name}>
         <td>{name}</td>
@@ -24,7 +23,7 @@ class WeatherList extends Component {
           </tr>
         </thead>
         <tbody>
-          { this.props.weather.map(this.renderWeather) }
+          { this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
